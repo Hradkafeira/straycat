7695b670ae97d2a109e5b6e54cbc5b294db1425a
hradkafeira <ariefakbarhidayat1098@gmail.com>
2022-06-23 12:54:40 +0700

    Bump version: 0.1.9 → 0.1.10

--- straycat/__init__.py
+++ straycat/__init__.py
@@ -6,6 +6,6 @@ A wrapper package for easy implementation of Indonesian NLP
 
 __author__ = """Arief Akbar Hidayat"""
 __email__ = 'hidayat.ariefakbar@gmail.com'
-__version__ = '0.1.9'
+__version__ = '0.1.10'
 
 from . import text_preprocessing  # noqa:F401
